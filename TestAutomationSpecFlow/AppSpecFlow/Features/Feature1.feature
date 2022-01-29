4eb7ad34c4cb4ca566ec19b9371c0d41ae90abcc
Mark Duffy <markiejduffy@gmail.com>
2022-01-29 09:45:15 +0000

    GivenButtonSteps introduced

---
+++ TestAutomationSpecFlow/AppSpecFlow/Features/Feature1.feature
@@ -0,0 +1,7 @@
+﻿@all @Pipeline1 @pipeline2 @AA000010
+Feature: AA000010-MenuTest
+	Test the Menu Structure, which does seem to tbe the biggest issue
+
+
+Scenario Outline: AA000010-0000 START
+	Given Button "X" Is Disabled
---
+++ TestAutomationSpecFlow/Generic/Steps/Button/GivenButtonSteps.cs
@@ -0,0 +1,17 @@
+﻿using Core.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace Generic.Steps.Button
+{
+    [Binding]
+    public class GivenButtonSteps
+    {
+        [Given(@"Button ""([^""]*)"" Is Disabled")]
+        public void GivenButtonIsDisabled(string x)
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
